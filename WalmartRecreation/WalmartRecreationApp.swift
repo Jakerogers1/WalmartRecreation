849700b9bd9383ad99583823df19b3219ff65486
Jake Rogers <jmanerr@ad.unc.edu>
2024-01-30 11:59:48 -0500

    Initial Commit

---
+++ WalmartRecreation/WalmartRecreationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalmartRecreationApp.swift
+//  WalmartRecreation
+//
+//  Created by jmanerr on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WalmartRecreationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
